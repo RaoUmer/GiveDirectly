f0f280c6c1e0f01b8dfa583926c35da62f3ce793
Neal <nealjean1@gmail.com>
2015-02-22 15:43:00 -0800

    Commented add_village_info.py script

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+# setuptools allows you to easily download, build, install, upgrade, and uninstall Python packages. Just import things from setuptools instead of distutils.
 try:
     from setuptools import setup
 except ImportError:
+    # ImportError is raised when an import statement fails to find the module definition
     from distutils.core import setup
 
-
+# Reads the entire contents of README.rst into readme variable
 readme = open('README.rst').read()
+# Reads in HISTORY.rst file and deletes the first line
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 requirements = [
